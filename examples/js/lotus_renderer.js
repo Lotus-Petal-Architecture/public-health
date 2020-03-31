38382c856336b90dc355f31fff376164eb1a07cc
tessgadwa <tessgadwa@gmail.com>
2020-03-30 21:44:11 -0700

    Added sample country values.

--- examples/js/lotus_renderer.js
+++ examples/js/lotus_renderer.js
@@ -53,14 +53,12 @@ var r = 100,
   var link_order = [] // list of all link values in the module, with k values assigned to each index position
   var k_values = [] // list of all k values generated for corresponding module chart lines
   var active_links = [] //index values of all active links
-  var countries = [] // values of countries from chart
-  var day_one = [] //number of cases on first day tracked
-  var day_ten = [] //number of cases on day 10
-
-
 
   var country_links = [] //index values of genre links
-  var song_names = ["OqeKV2UYq1Q", "kkcbxjWG9Mc", "MUfgAbFY4CA", "W05cPXpUHGI", "Bm1g5Yg0hUw", "iPAr7kL-mmg", "APrpB-i4d_E", "D1vQJFF2TKQ", "qR9DjdMrpHg", "E5H8DwJI0uA", "ksTFj6L0mao", "Kc1htX3q-F0", "GiZHmwzNAqE", "n7zyfArxibk", "vZA_7FtttRY", "hjg39XRkjVc", "E7fzUGR8ZH4", "ON6pn6suSzc", "W3m7Uz7hF-s", "CmBgxP56R1I", "QJu611UdfxA", "-DjpNgrocKo", "qOM107PIxV8", "FuFtfhOipNQ", "zJXQSBWO5Qc", "yQYu51hlkLk", "HwgNMrs-i80", "tRNDB9VqI3Q", "iLilpPtY2JU", "pO3_ZG7wJPc", "wcICuFnkxe4", "pnfryoGog0A", "hPsdjlPVaJU", "MbxRu7fwR24", "f-MroGCKDcM", "Mub2i2BoHpM", "6r1-HTiwGiY", "d9MA4rFNf7I", "E5uAH0vNn2s", "OCVgWq9B_HE", "4K8ou0iA_68", "PE1ges9nn6A", "w39qx5X_Owg", "aUfu-lEflbQ", "6P8mfvCGKyg", "TpLhrLzSaFQ", "rCy1VIy8Hj0", "ZyvYIYwLzTw", "cgr8e7da52o", "Cg4c0RA2DJQ", "f-FOTHUir_4", "BzkHp6EswEM", "YbP-Aa3V6bA", "7hneF9Iu71g", "FNFYq8O7DTY", "UNUmSwWq-LU", "4qljGaHJbCs", "2wnDyOHPxrE", "cr5uFjA4TNI", "MP_2p79Ems4", "-ITl4-Kyey0", "iG8D1Kb7xgQ", "au-mBoepJUA", "XMUxca7gXv4", "pB08AUiTP3w", "srwAMHbHVAE", "K_5lt23PRVs", "OFOowKu7WjA", "Ti1liRM6cao", "V4Yw6A_rlHc", "vtS54c9sP0U", "6RzlgEyS-BE", "Rh2YmGujtFI", "VTd4JCIqL7U", "uIP0iIxHLY4", "Pjw-qzT6qBs", "K0q6EYTGXXQ", "gK04XhlTLOM", "weW-VnINl-E", "QAhMakentwA", "IgfIh-NBoCw", "cwHmeFidLbE", "pGOO7EE4Lhw", "nKJeB03TrJg", "yMku_xfki1U", "WGnqoZx7_QY", "6SGy0zUCtsk", "X9BWRh92ifs", "9NrC7pRra9o", "E9yTHSyZeKg", "Z9RsVV-zfgY", "EIpzPVAHpVg", "LS7KFVYUQT4", "uS1PyjaR8WM", "WJi9MXfl3zA", "yntvBrlZNeA", "8QcTCIsFJ2Q", "RemMNe7z6vg", "4fr8k6O-Bko", "Ed_16Cblg9s", "Gv7iV4n7cXM", "uL8Dipf5kXc", "ClVNU2-C3rQ", "tAZko_MSZhc", "IbE4ynQd_qQ", "_P63qccOdzs", "zClCsQnRj-c", "uA4RNW3HkcQ", "Palxbwco9pM", "i94eFYHHAOY", "hBF8YGF17rQ", "bbt3qhOiH3M", "VkavEUCwm0M", "9Rz4kMBD8jU", "LJbtcit8Byg", "bS22uZHDr54", "bU2WUSEC6PY", "Duot03grNv8", "ull6hOYs5ZY", "ZEhWfrVOWlE", "W8CrBGhfSiA", "yHNB4m1dfKE", "GFfN5mFjOFo", "slua80kJ8zA", "AXHi-B2VbU0", "6Y_B3wdQLgg", "fPEoI43MMhs", "uJMn4WGaIOc", "rddu5TgrTmE", "SBrMBqFbWok", "DHXYSbs6Rb0", "s9t2QH5zTCQ", "qMol4iSzXis", "6u8lUKuaUx8", "_86LQH-c1d8", "c1UTArVwyZs", "HcB7ZnkMnB8", "slqKrANo7Uo", "0nt2Yn1M0oU", "yyayVIXwg74", "2bHvzuupe4w", "mKZBingy2OA", "aVwVvtP5qJ4", "s5Eyu9-kchQ", "Cqp-hL-I90A", "OV2lquaPxSU", "SBOK9CBjCK8", "9x6Mxs5DyxI", "GdK4jqnDNN8", "LesJtYAG8zM", "w_otXEVPgOk", "Pp_e6vZuhBI", "--w4Ui8Alzc", "cQhGxSge7aA", "rx4woNwVS8g", "50ADWH1d3E8", "eNcSFnr3508", "p1nmsaPh31E", "9IjxNwHkttU", "1jY7kdEIhaA", "YypAGqIBrX0", "eg5Emkcjfdo", "iF0IhEpDces", "FAL01pUnhGI", "DWeB6hWoKyI", "c8H7Anvad6E", "LH7XPoWPz-4", "TkIloV7OMAk", "_ZydMszfZlQ", "YGv-OSvQwKY", "AguOdYNF2d4", "TN9upnrVwog", "FxdnqfyvIkY", "-dJXBCBZwQg", "5AHz8HeDk3c", "DCI5XqT-AZs", "t7Pv3eZEy4k", "tG2F72T-ixY", "8drkE_zLnLk", "0deHAT_KOqE", "Q72ENpHcrDQ", "COICJal838M", "pfjyV0qtNlk", "sSCb-a2McRI", "M1hSddzlxL4", "RSfii8RLmNM", "rVqIhE53D_w", "UX1BvCRg6gs", "gahV15Oe9Xs", "pCgEUBf5y18", "vAuwQugGdS4", "jt068Vdmfww", "ENODBnQ5ed0", "JOUmxw0DPsg", "QYDXRoHpF0w", "-Rfqo7OSimw", "BVh6Jb3DQxE", "Ra616vyPBp8", "5xFP4ikGCLk", "HmuGq5weoZA", "KqXN_5G_kuo", "SVi7fXWKqSk", "fAcdBmxu-6s", "3jWQzkoPFTg", "nptjor9ee_Q", "rQfs5UTzwFQ", "wsF4TVHr42A", "xRFTYRXS3aw", "AwkDVMr4Kso", "H3GXCAeWMb0", "x9_1ia_nB_E", "Ys4YGRN8hgY", "t50c2AiAkpw", "q93hgeROqvk", "3zDO7P_P3Aw", "mfA9K1hj2eg", "0Jpqb5IYlEE", "BL8AJFQv9V4", "4n8vZyzBWNs", "1WaMgWUiYg0", "Hu0wknFNTOk", "iZtFjqZxBxw", "5SeI6r8lI_U", "A4uAL0T_CUI", "r6i5PCn7_oY", "ba_SMlx-gi0", "8Ux6UnYOLvk", "t1IMifXyJLc", "sk4aZSLSkMs", "XTCUjmhWL4Y", "2sKF3MHrHEY", "z0lHW09eQRA", "RFmIOaRhOnE", "H7vVXIiisSw", "-G1XWvUBXk4", "CyDHTJCIfHQ", "njHZdD0MGDs", "0_GeShK7aaY", "WWWKRqzvxMg", "_oQIIAdG8xM", "H_6GtBFCTyQ", "VZu1Z0oeFzo", "-Q53gXuXd5s", "oErErOGaAv0", "U3iWpewLuyA", "XPR2bV8u5rk", "_S0esU0n6sY", "U-eK9RcU90s", "KYhsehUH5b0", "9_N4gVkx9_w", "s_vgHgIKPQs", "Y6BeTnjUqlo", "kXFGQYGFeFU", "MP8Fd0mN50E", "qepl1N0P-SA", "bqogMblcal0", "_liZNFe-TDM", "SBA_vLLrXr0", "q-XpcMTnB-A", "c-kLsqvD6q8", "ZGmXbfF8uvM", "Bi4K58Fd_1o", "XfgAXwaseM8", "pOYN1p4Rc6o", "c4cBdT5WCoE", "599tHbAQVts", "8BhdoriXe9Q", "l5JhD4wKsrs", "iv0ej8cJScM", "uE2R5d6cPYs", "vxnYp5NyD4k", "aCgTgyBBswA", "C7r5KziEspU", "KR6DysiE9Sk", "trZ244Ih_E4", "71u8cSuyZrI", "DElGhE2NhtQ", "kYKcf7EWEfc", "KGKJQNtxjhI", "KiHrn43djYc", "qIhPPceIEV0", "U53SHxO4sbk", "zDoDWbB6RN8", "j0iohXlRXKA", "LEKxlNbjgmE", "6ZowS0dDW1k", "KLgWHoGLDx4", "V0HIfZmn3oU", "yDOi6phVQXM", "xxi6VQkCJPY", "bBMPzOX_VUo", "YZJ2rOm-PVA", "xuXln0HC-Lg", "ms2iYyh6jLI", "MJLe_O2J5Xg", "skvGTkW-qG4", "rFP4gxn_uME", "OLTeVRvPq04", "3MfJ9qMXBVQ", "xL94jLzIGt0"] //list of youtube videos. uses the same index ranking as link_order
+  var country_names = ["USA", "China", "Canada", "France", "Australia"] //list of countries, ranked in order of number of cases
+  var day_one = [1000, 100, 73, 29, 2] //number of cases on first day tracked
+  var day_ten = [1500, 950, 250, 72, 32] //number of cases on day 10
+
   var views = [] //popularity of youtube videas. uses same index ranking as link_order
   var song_titles = []
   var video_thmbs = []
@@ -461,13 +459,12 @@ assignLinks();
 function getActiveLinks()  //sorts for a given set of values from the data obtained above
 {
 
-    var f = song_names.entries(); 
+    var f = country_names.entries(); 
 
     for (x of f) {
       var song_value = x[1].toString();
       var song_index = x[0];
-      
-
+    
       if (countrygenre.includes(song_value)){
         
         country_links.push(song_index);
@@ -481,14 +478,13 @@ function addLinks() {  // adds links for selected values
 
 //document.getElementById("rock").style.backgroundColor = "#2387aa"
 
-
 addcountryLinks()
 
 
 parentTransform = new THREE.Object3D()
 group.add(parentTransform)
 
-for (i = 0; i < 304; i++) {  //this value should match entries.length
+for (i = 0; i < 300; i++) {  //this value should match entries.length
 
       var k = link_order[i];
       var color_code = 0xffffff;
@@ -508,7 +504,6 @@ for (i = 0; i < 304; i++) {  //this value should match entries.length
         parentTransform  //complete set of navigation links
       )
   }
-
 }
 
 
@@ -518,9 +513,8 @@ function geometricLinks () {
   addLinks();
 }
 
-// generates clickable and color-coded links sorted by genre and popularity
 
-function addTop8Songs() {  // adds links for selected values
+/*function addTop8Songs() {  // adds links for selected values
 
 document.getElementById("nowplaying").innerHTML = `<span style="font-size: 18px;font-family:Source Sans Pro; color: #FFF;"><strong>Top Eight Songs...</strong></span>`
 top8Transform = new THREE.Object3D()
@@ -547,7 +541,6 @@ for (i = 0; i < 8; i++) {
       )
   }
 top8Transform.visible = true
-
 }
 
 function addTopSongs() {  // adds links for selected values
@@ -576,23 +569,14 @@ for (i = 0; i < 100; i++) {
       )
   }
 topTransform.visible = true
-
-document.getElementById("nowplaying").innerHTML = `<span style="font-size: 18px;font-family:Source Sans Pro; color: #FFF; margin-left:0px;"><strong>Top 100 Songs...</strong></span>`
 }
+*/
 
-//setTimeout(spacer, 1000);
-//setTimeout(addTop8Songs, 200);
-//setTimeout(addTopSongs, 1200);
-setTimeout(geometricLinks, 1500);
-setTimeout('document.getElementById("nowplaying").innerHTML = `<span style="font-size: 18px;font-family:Source Sans Pro; color: #FFF; margin-left:0px; text-align:right; margin-top:10px;"></span>`'
-, 2000);
-setTimeout(spacer,5500);
-setTimeout(showDetails,6300);
+geometricLinks();
+showDetails();
 
 
-
-
-function getData() //processes JSON data and returns arrays for 5 main variables
+/*function getData() //processes JSON data and returns arrays for 5 main variables
   {
   var xmlhttp = new XMLHttpRequest();
   //xmlhttp.addEventListener("load", getActiveLinks);
@@ -606,10 +590,10 @@ function getData() //processes JSON data and returns arrays for 5 main variables
   if (this.readyState == 4 && this.status == 200) {
     var entries = this.response;
     //console.log(entries.length);
-    song_names.length = 0;
+    country_names.length = 0;
     if(entries.length > 0) {
     for (var i = 0; i < entries.length; i++) {
-    //console.log (song_names.length) // current actual number of songs being ranked
+    //console.log (country_names.length) // current actual number of songs being ranked
       var song = entries[i];
       var song_name = song.id;
       var popularity = song.statistics.viewCount;
@@ -617,7 +601,7 @@ function getData() //processes JSON data and returns arrays for 5 main variables
   //console.log(popularity);
   //console.log(song_title);
       views.push([popularity]);
-      song_names.push([song_name]);
+      country_names.push([song_name]);
       song_titles.push([song_title]);
       video_thmbs[video_thmbs.length] = song.snippet.thumbnails.default.url;
   //console.log (video_thmbs)
@@ -629,14 +613,11 @@ function getData() //processes JSON data and returns arrays for 5 main variables
   }
 }
 
-getData();
+getData();*/
 
 
 
 
-
-var URL = "https://www.youtube.com/embed/" + "-Rfqo7OSimw" + "?autoplay=1&mute=1"
-window.open(URL, 'iframe_a')
 
 function showDetails()
 
@@ -744,7 +725,6 @@ function hideControls() {
   document.getElementById("rank").style.visibility  = "hidden";
   document.getElementById("share").style.visibility  = "hidden";
   document.getElementById("toggleControls").style.backgroundImage  = "url('examples/files/maximize_icon.png')";
-
 }
 
 function showControls() {
@@ -944,7 +924,7 @@ function hideAll() {
       obj = intersection.object
       k = obj.label
       l = link_order.indexOf(k)   //connects the k value -- position on lotus petal graph -- to ID for link value
-      var URL = "https://www.youtube.com/embed/" + song_names[l] + "?autoplay=1&mute=0"
+      var URL = "https://www.youtube.com/embed/" + country_names[l] + "?autoplay=1&mute=0"
       window.open(URL, 'iframe_a')
     }
   }
